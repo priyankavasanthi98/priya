c0eaab26229e31adaff6291652d8a76b7d391460
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-09 19:11:09 +0530

    Create polindrom no.c

---
+++ polindrom no.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=181,rem,rev=0,t;
+printf("%d",n);
+t=n;
+while(n!=0)
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+if(t==rev)
+{
+printf("\n yes the number is palindrome",n);
+}
+else
+{
+printf("\n the number is not palindrome",n);
+}
+}
