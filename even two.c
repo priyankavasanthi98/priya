b334bb0b94266023f699de2115eda89fad6c5a25
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-10 16:21:47 +0530

    Create even two.c

---
+++ even two.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a=1,b=9;
+printf("%d first interval",a);
+printf("%d second interval",b);
+for(i=a;i<=b;i++)
+{
+if(i%2==0)
+{
+printf("%d the even number between two interval is");
+}
+else
+{
+printf("%d there is no even number between two interval");
+}
+}
+}
