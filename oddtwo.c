eda5f229e9763ad1821936a9535a7a4166dd7228
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-10 15:54:01 +0530

    Update oddtwo.c

--- oddtwo.c
+++ oddtwo.c
@@ -2,9 +2,9 @@
 #include<conio.h>
 void main()
 {
-int i,a,b;
-printf("enter the first interval %d",&a);
-printf("enter the second interval %d",&b);
+int i,a=2,b=9;
+printf("enter the first interval %d",a);
+printf("enter the second interval %d",b);
 for(i=a;i<=b;i++)
 {
 if(i%2==1)
