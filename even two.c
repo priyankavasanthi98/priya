37504cab6ba98909230d3b5ea675600b821642d1
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-10 16:27:09 +0530

    Update even two.c

--- even two.c
+++ even two.c
@@ -3,12 +3,12 @@
 void main()
 {
 int i,a=1,b=9;
-printf("%d first interval",a);
-printf("%d second interval",b);
+printf("\n %d first interval",a);
+printf("\n %d second interval",b);
 for(i=a;i<=b;i++)
 {
 if(i%2==0)
 {
-printf("%d the even number between two interval is");
+printf("%d the even number between two intervals are",i);
 }
 }
