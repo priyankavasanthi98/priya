69d9cf446bf479746aaaed988e81fc71501e36ce
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-19 15:39:15 +0530

    Update arm two.c

--- arm two.c
+++ arm two.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 void main()
-{int n,i,t,a=120,b=130,rem,result=0;
+{int n,i,t,a=150,b=160,rem,result=0;
 printf("the first interval %d",a);
 printf("the second interval %d",b);
 {
-for(i=a;i<=b;i++)
+for(i=150;i<=160;i++)
 {
 t=n;
 while(n!=0)
@@ -16,7 +16,7 @@ n=n/10;
 }
 if(t==result)
 {
-printf("armstrong number between two intervals are %d ",a,b);
+printf("armstrong number between two intervals are %d ",n);
 }
 }
 }
