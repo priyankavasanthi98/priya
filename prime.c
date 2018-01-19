4a0a0a9acc3962e1a3299482bbdd706806d0cf12
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-19 18:54:34 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=1,b=10,n,i,count=0;
+printf("\n the first interval is %d",a);
+printf("\n the second interval is %d",b);
+for(i=2;1<n;i++)
+{
+if((n%i)==0)
+count++;
+}
+if(count==0)
+{
+printf("\n the prime number between two intervals are");
+}
+}
