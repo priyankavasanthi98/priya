1a25c96ae13a05ace5254886e61b207de7923a6f
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-21 09:46:10 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -2,18 +2,22 @@
 #include<conio.h>
 void main()
 {
-int a=1,b=10,j,i;
+int a=1,b=10,j,i,count;
 printf("\n the first interval is %d",a);
 printf("\n the second interval is %d",b);
-for(i=a;1<b;++i)
-{count=0;
-  for(j=2;j<i/2;++j)
-  {
+for(i=a;i<b;++i)
+{
+ count=0;
+for(j=2;j<i/2;++j)
+{
 if((i%j)==0)
+{
 count=1;
 }
 if(count==0)
 {
-printf("\n the prime number between two intervals are");
+printf("\n the prime number between two intervals are",&i);
+}
+}
 }
 }
