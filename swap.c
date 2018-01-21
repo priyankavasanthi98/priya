245659de806c8a252ae6ae30b5e5c70da2d42bec
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-21 12:21:37 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int m,n;
+printf("\n %d enter the 1st no:",&a);
+printf("\n %d enter the 2nd no:",&b);
+m=m-n;
+n=m+n:
+m=n-m;
+printf("\n before swapping %d",&a,&b);
+printf("\n after swapping %d",&a,&b);
+}
