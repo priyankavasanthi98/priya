628ff7386b2458085322a1e102e80ee139783cba
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-21 09:26:33 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -2,15 +2,15 @@
 #include<conio.h>
 void main()
 {
-int a=1,b=10,j,i,count=0;
+int a=1,b=10,j,i;
 printf("\n the first interval is %d",a);
 printf("\n the second interval is %d",b);
-for(i=a;1<b;i++)
-{
-  for(j=2;j<i;j++)
+for(i=a;1<b;++i)
+{count=0;
+  for(j=2;j<i/2;++j)
   {
 if((i%j)==0)
-count++;
+count=1;
 }
 if(count==0)
 {
