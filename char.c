9a55ccb7d86953bfffbc5c18119a01b8abcf33b1
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-21 10:21:45 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i,count;
+char a[20];
+printf("%s enter the string",a);
+gets(str);
+for(i=1;i<=a;i++)
+{
+count++;
+printf("%s",&a);
+}
+}
