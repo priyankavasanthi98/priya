b3f77c67648a4bac89c85548314d708437368311
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-21 11:52:35 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+char ch;
+printf("%c",&n);
+if((ch>=65&&ch<=90)!!(ch>=97&&ch<=122))
+{
+printf("%d number",n);
+}
+else
+{
+printf("%d not a number",n);
+}
+}
