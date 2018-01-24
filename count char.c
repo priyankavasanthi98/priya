90da131bb445896f2067eeffd5fc3d7c090f00ed
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-24 19:34:14 +0530

    Create count char.c

---
+++ count char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+char str[200],ch;
+int count=0,i=0;
+printf("enter the string ");
+scanf("%d",str[200])
+printf("count the number of character:\n");
+scanf("%c",&ch);
+while(str [i]='\0')
+{
+if (str[i]==ch)
+count++;
+i++;
+}
+printf("count number of characters in the string ");
+}
