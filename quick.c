d94de248e9cb1d461e87953b0fbb393730384ffd
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-25 18:53:42 +0530

    Create quick.c

---
+++ quick.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,a[10],s,m;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=1;j<=n;j++)
+{
+for(k=j+1;k<=n;m++)
+{
+if(a[j]>=a[m])
+{
+s=a[j];
+a[j]=a[m];
+a[m]=s;
+}
+}
+}
+printf("%d",s);
+return 0;
+}
