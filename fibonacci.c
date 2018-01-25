dd54b734377c095f0df54f345fa83aecb9000601
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-01-25 19:41:02 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,t1=0,t2=1,n,t;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d",t1);
+nt=t1+t2;
+t1=t2;
+t2=nt;
+}
+return0;}
