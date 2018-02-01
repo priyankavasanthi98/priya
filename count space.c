6654ad1862f87f30e718cc20faf5e7739087e9e2
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-01 19:37:50 +0530

    Update count space.c

--- count space.c
+++ count space.c
@@ -5,9 +5,9 @@ char str[40];
 int counter,count=0;
 printf("enter the string:");
 gets(str);
-for(counter=0;str[counter]!=null;count++)
+for(counter=0;str[counter]!=NULL;counter++)
 {
-if(str[counter]=' ')
+if(str[counter]==' ')
 count++;
 }
 printf("%d",count);
