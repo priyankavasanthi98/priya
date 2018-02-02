d6dec8b4a3c4123929de3860bf4988b765d68b33
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-02 11:14:33 +0000

    Create special.c

---
+++ special.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+char str[20];
+int i,count=0;
+printf("enter the string:");
+gets(str);
+for(i=0;ster[i]!=NULL;i++)
+{
+if((str[i]>=0)&&(str[i]<=9))
+count++;
+{
+elseif((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))
+count++;
+{
+elseif(str[i]=' ')
+count++
+{
+else
+count++;
+}
+}
+}
+}
+printf("%d",count);
+}
