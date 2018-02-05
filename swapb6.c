7485344bdb56c06e8c44338ac02271e77bbc70fa
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-05 11:21:53 +0000

    Create swapb6.c

---
+++ swapb6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("values of a&b before swapping:",a,b);
+scanf("%d %d",&a,&b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("values of a&b after swapping%d %d",a,b);
+}
+
