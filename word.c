52203fb5cb8120428a122501a1f90e5474f4ce4e
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-05 11:08:31 +0000

    Create word.c

---
+++ word.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char s[10];
+int i=0;f=1;
+printf("enter the string:");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(count==' ')
+{
+f++;
+}
+i++;
+}
+printf("%d",i);
+}
