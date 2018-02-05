e9dd59791685038b4ac43897d79b92f838a77355
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-05 10:43:21 +0000

    Update maximum.c

--- maximum.c
+++ maximum.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 void main()
 {
-int a[3],i,j,t;
+int i,j,t;
+  int a[3]={1,2,3};
   for(i=0;i<3;i++)
     
   {
     for(j=i+1;j<3;j++)
     {
-      if(a[i]>a[j])
+      if(a[i]<a[j])
       {
         t=a[i];
         a[i]=a[j];
         a[j]=t;
       }
     }
-    printf(%d",a[i]);
-           }
+  }
+    printf(%d",a[0]);
+           
            }
