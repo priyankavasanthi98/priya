5600f7fbcd4bae6fcf566f6521a4665741df54e1
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-09 10:26:13 +0000

    Create print41.c

---
+++ print41.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[30];
+int i,k=3;
+printf("enter the string:");
+scanf("%s",&s[i]);
+for(i=0;i<=k;i++)
+{
+printf("hello world",k);
+}
+}
