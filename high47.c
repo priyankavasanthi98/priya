96d294cd14e59ab50d21229f4d7e7e449b51a7a3
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-09 11:06:59 +0000

    Create high47.c

---
+++ high47.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,j,n,t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("%d %d",a[n-1],a[n=0]);
+}
