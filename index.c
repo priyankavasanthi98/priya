63d9962f79a8e2578c5f4cc9423ded9bcd104050
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-09 11:30:49 +0000

    Update index.c

--- index.c
+++ index.c
@@ -3,11 +3,14 @@ void main()
 {
 int a[10],n,i;
 printf("\n enter the size of the array");
-scanf("%d",&a[10]);
-printf("\n enter the elements");
-scanf("%d",&t);
-for(i=0;i<ni++)
+scanf("%d",&n);
+for(i=0;i<n;i++)
 {
-printf("%d%d",i,n);
+printf("\n enter the elements");
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+printf("%d %d \n",a[i],i);
+}
 }
 }
