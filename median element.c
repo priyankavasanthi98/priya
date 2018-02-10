e4aeb8bb94d151e879002d3d0f76439ef84210fb
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-10 16:59:56 +0530

    Update median element.c

--- median element.c
+++ median element.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int i,j,temp,median,n=3;
+int i,j,temp,median,n;
+scanf("%d",&n);
 int a[3]={3,2,1};
-for(i=0;i<5;i++)
+for(i=0;i<n;i++)
 {
-for(j=i+1;j<3;j++)
+for(j=i+1;j<n;j++)
 {
 if(a[i]>a[j])
 {
@@ -15,13 +16,13 @@ a[j]=temp;
 }
 }
 }
-for(i=0;i<3;i++)
+for(i=0;i<n;i++)
 {
 printf("%d \n",a[i]);
 }
-for(i=0;i<3;i++)
+for(i=0;i<n;i++)
 {
-median=(n+1)/((n+1)/2);
+median=a[n/2];
 printf("\n %d",median);
 }
 }
