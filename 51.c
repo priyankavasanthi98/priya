f391c22da39f388623b36fe49e403a3a3c7528d3
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-21 19:29:58 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,a,b,sum=0,s=0;
+scanf("%d",&n);
+while(n!=0)
+{
+a=n%10;
+sum=sum*10+a;
+n=n/10;
+}
+while(sum!=0)
+{
+b=sum%10;
+printf("%d\t",s);
+sum=sum/10;
+}
+}
