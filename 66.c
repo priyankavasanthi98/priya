6ee55fb4922c95c78a45fe49eefab05f0a708578
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-26 16:40:35 +0530

    Update 66.c

--- 66.c
+++ 66.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 void main()
 {
-int n,t,
+int n,t,c=0;
+  scanf("%d",&n);
+  for(i=2;i<n;i++)
+  {
+    if(n%i==1)
+    c++;
+  }
+  if(c==0)
+  {
+    printf("prime");
+  }
+  else
+  {
+    printf("not prime");
+  }
+}
