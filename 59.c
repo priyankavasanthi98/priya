64db9cd2c9dad0ea212ae9a9c540822b19895b89
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-26 16:03:39 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int n=10,i,j,t;
+scanf("%d",&n);
+for(i=0;i<10;i++)
+{
+scanf("%d",&n);
+}
+for(i=0;i<10;i++)
+{
+for(j=i+1;j<10;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=a[i];
+}
+}
+}
+printf("%d",a[0]);
+}
+
