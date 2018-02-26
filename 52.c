c7194ad5aa96a1738a9d5cc3270b1fc32e337e0d
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-26 15:54:05 +0530

    Update 52.c

--- 52.c
+++ 52.c
@@ -5,38 +5,27 @@ int ch;
 scanf("%d",&ch);
 switch(ch)
 {
-case 1:
-printf("one");
-break;
-case 2:
-printf("two");
-break;
-case 3:
-printf("three");
-break;
-case 4:
-printf("four");
-break;
-case 5:
-printf("five");
-break;
-case 6:
-printf("six");
-break;
-case 7:
-printf("seven");
-break;
-case 8:
-printf("eight");
-break;
-case 9:
-printf("nine");
-break;
-case 10:
-printf("ten");
-break;
-printf("enter any number");
-default:
+case 1:printf("one");
+       break;
+case 2:printf("two");
+       break;
+case 3:printf("three");
+       break;
+case 4:printf("four");
+       break;
+case 5:printf("five");
+       break;
+case 6:printf("six");
+       break;
+case 7:printf("seven");
+       break;
+case 8:printf("eight");
+       break;
+case 9:printf("nine");
+       break;
+case 10:printf("ten");
+       break;
+default:printf("number should be within limit 1 to 10");
 break;
 }
 }
