548dc5deea7cab515e1cb1ceb37e9f22e6631677
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-28 15:55:05 +0530

    Update 63.c

--- 63.c
+++ 63.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 void main()
 {
 char str[30];
-int i,c=0;
+int i,c=1;
 printf("enter the string:");
 gets(str);
 for(i=0;str[i]!=NULL;i++)
 {
 if(str[i]==' ')
 {
-c++;
+c=c+1;;
+}
 }
 printf("%d",c);
-}
 }
