9d3af5cece7fc7026ab809e6dbe7c790e9b91700
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-28 16:47:36 +0530

    Update 72.c

--- 72.c
+++ 72.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 void main()
 {
-char ch;
+char ch[60];
+int i;
 scanf("%c",&ch);
+for(i-0;ch[i]!='\0';i++)
+{
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
 printf("yes");
+  break;
 }
 else
 {
 printf("no");
+  break;
+}
 }
 }
