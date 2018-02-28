7f10e8e1ef09584e1c1f1cdccf30e160b0ccac33
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-02-28 16:48:21 +0530

    Update 72.c

--- 72.c
+++ 72.c
@@ -5,9 +5,9 @@ void main()
 char ch[60];
 int i;
 scanf("%c",&ch);
-for(i-0;ch[i]!='\0';i++)
+for(i=0;ch[i]!='\0';i++)
 {
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
 printf("yes");
   break;
