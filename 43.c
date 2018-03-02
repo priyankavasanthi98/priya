43a15c32103893bbf93cd60d653cb0f84e41fd66
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-03-02 16:22:26 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s1[20],s2[20];
+int i,j;
+printf("enter the first string:");
+scanf("%s",&s1);
+printf("enter the second string:");
+scanf("%s",&s2);
+while(s1[i]!=NULL)
+{
+i++;
+}
+while(s2[j]!=NULL)
+{
+s[i]=s[j];
+i++;
+j++;
+}
+printf("%s",s1);
+}
