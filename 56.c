102b686fae086fb6a180f059bea8f3d6fb6680d4
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2018-03-12 16:34:24 +0530

    Update 56.c

--- 56.c
+++ 56.c
@@ -2,15 +2,15 @@
 #include<string.h>
 void main()
 {
-char c[100];
+char str[100];
 int i,n=0,a=0;
 printf("enter the string:");
-scanf("%c",&c);
+scanf("%s",&str);
 if(str[i]>=0||str[i]<=9)
 n++;
 if((str[i]>=65&&str[i]<=90)||(str[i]>=97&&str[i]<=122))
 a++;
-if(n>=a)
+if(n!=0&&a!=0)
 {
 printf("yes");
 }
